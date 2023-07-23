bead66a9567c0db057151b886a198ea3eb45340a
tertius Jubber <tjubber@gmail.com>
2023-07-23 11:30:02 +0200

    fullstack open

---
+++ server.js
@@ -0,0 +1,77 @@
+const express = require('express')
+const app = express()
+const PORT = 8000
+
+app.use(express.json())
+
+let persons = [
+    { 
+      "id": 1,
+      "name": "Arto Hellas", 
+      "number": "040-123456"
+    },
+    { 
+      "id": 2,
+      "name": "Ada Lovelace", 
+      "number": "39-44-5323523"
+    },
+    { 
+      "id": 3,
+      "name": "Dan Abramov", 
+      "number": "12-43-234345"
+    },
+    { 
+      "id": 4,
+      "name": "Mary Poppendieck", 
+      "number": "39-23-6423122"
+    }
+]
+
+app.get('/api/persons',(request,response)=>{
+  response.json(persons)
+})
+
+app.get('/info',(req,res)=>{
+  const currentDate = new Date()
+
+  res.send(`<h2>Phonebook has info for ${persons.length} people</h2> <h2>${currentDate} </h2>` )
+})
+
+app.get("/api/persons/:id",(req,res)=>{
+  const id = +(req.params.id)
+ const entry = persons.find(entry => entry.id == id)
+ if(entry){
+  res.json(entry)
+ }else{
+  res.status(404).end()
+ }
+})
+
+app.delete("/api/persons/:id",(req,res) => {
+  const id = Number(req.params.id)
+  persons = persons.filter(entry => entry.id !== id)
+  res.status(204).end()
+})
+
+const generateId = () => {
+  const maxId =persons.length > 0 ? Math.max( ...persons.map(n => n.id)) : 0
+  return maxId +1
+}
+
+app.post('/api/persons',(req,res) => {
+  const body = req.body
+
+  let entry = {
+    id: generateId(),
+    name: body.name,
+    number: body.number
+  }
+  res.json(entry)
+  persons.push(entry)
+})
+
+app.listen(process.env.PORT || PORT,()=>{
+  console.log(`server running on Port ${PORT}`);
+})
+
+
